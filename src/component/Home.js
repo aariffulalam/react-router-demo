0529796b289ae3a85b179c0763bcc5c24a938828
aariffulalam <aarif20@navgurukul.org>
2022-07-01 14:57:03 +0530

    navigation applied for to go to the previous page.

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,8 +4,9 @@ function Home(){
     const navigate =  useNavigate()
     return (
         <>
-        <h3>Home Page</h3>
-        <button onClick={()=>{navigate('ordersummary')}}>Order Now</button>
+            <h3>Home Page</h3>
+            <button onClick={()=>{navigate('ordersummary', {replace:true})}}>Order Now</button>
+            {/* this replace will be remove the history if we will navigate to other page and wants to come back to this page this will not be come we will go to the google root page. */}
         </>
     )
 }
--- src/component/OrderSummary.js
+++ src/component/OrderSummary.js
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function OrderSummary(){
+    const navigate = useNavigate()
     return (
-        <h3>Order confirmed !</h3>
+        <>
+            <h3>Order confirmed !</h3>
+        <button onClick={()=>{navigate(-1)}}>Go back</button>
+        {/* this button will allow us to go back to the previous page. */}
+
+        </>
     )
 }
 
