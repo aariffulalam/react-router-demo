08cbcc65a09dbed6050579df14858a8c3a4fc95b
aariffulalam <aarif20@navgurukul.org>
2022-07-01 18:40:49 +0530

    Dynamic Routing using UserSearchParams

--- src/component/Users.js
+++ src/component/Users.js
@@ -1,5 +1,9 @@
 import { Outlet } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
+
 export const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const showActiveUsers = searchParams.get("filter") === "active"
   return (
     <div>
         <ul>
@@ -8,7 +12,13 @@ export const Users = () => {
             <li>user 3</li>
         </ul>
     <Outlet/>
-
+      <div>
+        <button onClick={()=>setSearchParams({filter:"active"})}>Active users</button>
+        <button onClick={()=>setSearchParams({})}>Reset</button>
+      </div>
+      {
+        showActiveUsers ? <h3>Active users</h3>: <h4>All Users</h4>
+      }
     </div>
   )
 }
